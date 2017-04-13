a38c91e39702c655f12df9f9f47644fbd17a83a7
Tim Strijdhorst <tim@decorrespondent.nl>
2017-04-13 15:50:25 +0200

    %refactor connecting server code to protected function %use short array syntax where possible

---
+++ src/Driver/Redis.php
@@ -0,0 +1,308 @@
+<?php
+
+/*
+ * This file is part of an alternative driver package and is not part of the Stash Package.
+ * It is however derived from that code.
+ */
+
+namespace Respondens\Stash\Driver;
+
+use Stash\Driver\AbstractDriver;
+use Stash\Utilities;
+
+/**
+ * The Redis driver is used for storing data on a Redis system. This class uses
+ * the PhpRedis extension to access the Redis server.
+ *
+ * @package Stash
+ * @author  Robert Hafner <tedivm@tedivm.com>
+ */
+class Redis extends AbstractDriver {
+	protected static $pathPrefix = 'pathdb:';
+	
+	/**
+	 * The Redis drivers.
+	 *
+	 * @var \Redis|\RedisArray
+	 */
+	protected $redis;
+	
+	/**
+	 * The cache of indexed keys.
+	 *
+	 * @var array
+	 */
+	protected $keyCache = array();
+	
+	protected $redisArrayOptionNames = array(
+		"previous",
+		"function",
+		"distributor",
+		"index",
+		"autorehash",
+		"pconnect",
+		"retry_interval",
+		"lazy_connect",
+		"connect_timeout",
+	);
+	
+	/**
+	 * The options array should contain an array of servers,
+	 *
+	 * The "server" option expects an array of servers, with each server being represented by an associative array. Each
+	 * redis config must have either a "socket" or a "server" value, and optional "port" and "ttl" values (with the ttl
+	 * representing server timeout, not cache expiration).
+	 *
+	 * The "database" option lets developers specific which specific database to use.
+	 *
+	 * The "password" option is used for clusters which required authentication.
+	 *
+	 * @param array $options
+	 */
+	protected function setOptions(array $options = array()) {
+		$options += $this->getDefaultOptions();
+		
+		// Normalize Server Options
+		if (isset($options['servers'])) {
+			$unprocessedServers = (is_array($options['servers'])) ? $options['servers'] : [$options['servers']];
+			unset($options['servers']);
+			
+			$servers = $this->processServerConfigurations($unprocessedServers);
+		}
+		else {
+			$servers = array(array('server' => '127.0.0.1', 'port' => '6379', 'ttl' => 0.1));
+		}
+		
+		// this will have to be revisited to support multiple servers, using
+		// the RedisArray object. That object acts as a proxy object, meaning
+		// most of the class will be the same even after the changes.
+		
+		if (count($servers) == 1) {
+			$server = $servers[0];
+			$redis  = new \Redis();
+			
+			if (isset($server['socket']) && $server['socket']) {
+				$redis->connect($server['socket']);
+			}
+			else {
+				$port = isset($server['port']) ? $server['port'] : 6379;
+				$ttl  = isset($server['ttl']) ? $server['ttl'] : 0.1;
+				$redis->connect($server['server'], $port, $ttl);
+			}
+			
+			// auth - just password
+			if (isset($options['password'])) {
+				$redis->auth($options['password']);
+			}
+			
+			$this->redis = $redis;
+		}
+		else {
+			$redisArrayOptions = array();
+			foreach ($this->redisArrayOptionNames as $optionName) {
+				if (array_key_exists($optionName, $options)) {
+					$redisArrayOptions[$optionName] = $options[$optionName];
+				}
+			}
+			
+			$serverArray = array();
+			foreach ($servers as $server) {
+				$serverString = $server['server'];
+				if (isset($server['port'])) {
+					$serverString .= ':'.$server['port'];
+				}
+				
+				$serverArray[] = $serverString;
+			}
+			
+			$redis = new \RedisArray($serverArray, $redisArrayOptions);
+		}
+		
+		// select database
+		if (isset($options['database'])) {
+			$redis->select($options['database']);
+		}
+		
+		$this->redis = $redis;
+	}
+	
+	/**
+	 * Properly close the connection.
+	 */
+	public function __destruct() {
+		if ($this->redis instanceof \Redis) {
+			try {
+				$this->redis->close();
+			}
+			catch (\RedisException $e) {
+				/*
+				 * \Redis::close will throw a \RedisException("Redis server went away") exception if
+				 * we haven't previously been able to connect to Redis or the connection has severed.
+				 */
+			}
+		}
+	}
+	
+	/**
+	 * {@inheritdoc}
+	 */
+	public function getData($key) {
+		return unserialize($this->redis->get($this->makeKeyString($key)));
+	}
+	
+	/**
+	 * {@inheritdoc}
+	 */
+	public function storeData($key, $data, $expiration) {
+		$store = serialize(array('data' => $data, 'expiration' => $expiration));
+		if (is_null($expiration)) {
+			return $this->redis->set($this->makeKeyString($key), $store);
+		}
+		else {
+			$ttl = $expiration - time();
+			
+			// Prevent us from even passing a negative ttl'd item to redis,
+			// since it will just round up to zero and cache forever.
+			if ($ttl < 1) {
+				return true;
+			}
+			
+			return $this->redis->setex($this->makeKeyString($key), $ttl, $store);
+		}
+	}
+	
+	/**
+	 * {@inheritdoc}
+	 */
+	public function clear($key = null) {
+		if (is_null($key)) {
+			$this->redis->flushDB();
+			
+			return true;
+		}
+		
+		$pathString = $this->makeKeyString($key, true);
+		$keyString  = $this->makeKeyString($key);
+		$this->redis->incr($pathString); // increment index for children items
+		$this->redis->delete($keyString); // remove direct item.
+		$this->deleteSubKeys($keyString);
+		$this->keyCache[$pathString] = array();
+		
+		return true;
+	}
+	
+	/**
+	 * {@inheritdoc}
+	 */
+	public function purge() {
+		return true;
+	}
+	
+	/**
+	 * {@inheritdoc}
+	 */
+	public static function isAvailable() {
+		return class_exists('Redis', false);
+	}
+	
+	/**
+	 * Turns a key array into a key string. This includes running the indexing functions used to manage the Redis
+	 * hierarchical storage.
+	 *
+	 * @param  array $key
+	 * @param  bool  $path
+	 * @return string
+	 */
+	protected function makeKeyString($key, $path = false) {
+		$key = Utilities::normalizeKeys($key);
+		
+		$keyString = '';
+		foreach ($key as $name) {
+			$keyString .= $name;
+			
+			/*
+			 * Check if there is an index available in the pathdb, that means there was a deletion of the stackparent before
+			 * and we should use the index inside the pathdb to as a prefix for the sub-keys.
+			 */
+			$pathString = self::$pathPrefix.$keyString;
+			if (isset($this->keyCache[$pathString])) {
+				$index = $this->keyCache[$pathString];
+			}
+			else {
+				$index = $this->redis->get(self::$pathPrefix.$keyString);
+			}
+			
+			if ($index) {
+				$keyString .= '_'.$index;
+			}
+			
+			$keyString .= ':';
+		}
+		
+		$keyString = rtrim($keyString, ':');
+		
+		return $path ? self::$pathPrefix.$keyString : $keyString;
+	}
+	
+	/**
+	 * @param string $keyString
+	 */
+	private function deleteSubKeys($keyString) {
+		$iterator = null;
+		
+		while ($subKeys = $this->redis->scan($iterator, $keyString.'*')) {
+			foreach ($subKeys as $subKey) {
+				$this->redis->delete($subKey);
+			}
+		}
+	}
+	
+	/**
+	 * {@inheritdoc}
+	 */
+	public function isPersistent() {
+		return true;
+	}
+	
+	/**
+	 * @param array $unprocessedServers
+	 * @return array
+	 */
+	protected function processServerConfigurations(array $unprocessedServers) {
+		$servers = [];
+		foreach ($unprocessedServers as $server) {
+			$ttl = '.1';
+			if (isset($server['ttl'])) {
+				$ttl = $server['ttl'];
+			}
+			elseif (isset($server[2])) {
+				$ttl = $server[2];
+			}
+			
+			if (isset($server['socket'])) {
+				$servers[] = array('socket' => $server['socket'], 'ttl' => $ttl);
+			}
+			else {
+				$host = '127.0.0.1';
+				if (isset($server['server'])) {
+					$host = $server['server'];
+				}
+				elseif (isset($server[0])) {
+					$host = $server[0];
+				}
+				
+				$port = '6379';
+				if (isset($server['port'])) {
+					$port = $server['port'];
+				}
+				elseif (isset($server[1])) {
+					$port = $server[1];
+				}
+				
+				$servers[] = array('server' => $host, 'port' => $port, 'ttl' => $ttl);
+			}
+		}
+		
+		return $servers;
+	}
+}
